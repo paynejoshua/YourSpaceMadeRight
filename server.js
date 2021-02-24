d1052ccaadc0b6bc280d624f4b54fd1126642012
Josh Payne <paynejoshua1@gmail.com>
2021-02-24 12:48:06 -0800

    trying to get working on heroku

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-let express = require('express')
-
-let app = express()
-
-app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
-  });
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', (req, res) => {
+res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.listen(port, () => {
+console.log('Server is up!');
+});
